db7533b0233d505e0516096135078e7c2260cdaf
RumbosE <117108052+RumbosE@users.noreply.github.com>
2024-04-30 09:50:31 -0400

    Add new course pages and components

--- src/app/home/home.routes.ts
+++ src/app/home/home.routes.ts
@@ -3,6 +3,9 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 import { LayoutComponent } from './pages/layout/layout.component';
 import { SettingsPageComponent } from './pages/settings-page/settings-page.component';
+import { MainCourseComponent } from './pages/courses-pages/main-course/main-course.component';
+import { VideoListComponent } from './pages/courses-pages/video-list/video-list.component';
+import { PlayerVideoComponent } from './pages/courses-pages/player-video/player-video.component';
 
 export const HomeRoutes: Routes = [
   {
@@ -21,6 +24,18 @@ export const HomeRoutes: Routes = [
       {
         path: 'settings',
         component: SettingsPageComponent
+      },
+      {
+        path: 'main-course',
+        component: MainCourseComponent
+      },
+      {
+        path: 'video-list',
+        component: VideoListComponent
+      },
+      {
+        path: 'player-video',
+        component: PlayerVideoComponent
       },
     ]
   }
---
+++ src/app/home/pages/courses-pages/video-list/video-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-video-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './video-list.component.html',
+  styleUrl: './video-list.component.css'
+})
+export class VideoListComponent {
+
+}
