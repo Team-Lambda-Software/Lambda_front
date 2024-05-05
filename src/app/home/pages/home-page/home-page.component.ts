57866b5c1798f786a8417dea575388bc07f2f4c9
highwir3 <hollowsouls2001@gmail.com>
2024-05-04 20:11:53 -0400

    Added video courses and latest blog sliders to home, template my training slider in profile

--- src/app/home/pages/home-page/home-page.component.ts
+++ src/app/home/pages/home-page/home-page.component.ts
@@ -3,10 +3,31 @@ import { CommonModule } from '@angular/common';
 import { RouterLink, Router, ActivatedRoute } from '@angular/router';
 import { CarruselBgImgComponent } from '../../components/carrusel-bg-img/carrusel-bg-img.component';
 
+interface IUser {
+  id: string;
+  user: string;
+  image: string;
+}
+
 interface IPopularCourses {
   id: number;
   teacher: string;
   category: string;
+  image: string;
+}
+
+interface IVideoCourses {
+  id: number;
+  title: string;
+  description: string;
+  thumbnail: string;
+  videoUrl: string;
+}
+
+interface ILatestBlogs {
+  id: number;
+  name: string;
+  description: string;
   image: string;
 }
 
@@ -23,12 +44,48 @@ export class HomePageComponent {
   public days: string[] = ['Tomorrow', 'Today', 'Yesterday'];
   public selectedDays: string = 'Today';
   public popularCourses: IPopularCourses[] = [
-    { id: 1, teacher: 'Eduardo', category: 'Prenatal' , image: 'https://via.placeholder.com/250' },
-    { id: 2, teacher: 'Paul', category: 'Prenatal' , image: 'https://via.placeholder.com/250' },
-    { id: 3, teacher: 'Alfredo', category: 'Prenatal' , image: 'https://via.placeholder.com/250' },
-    { id: 4, teacher: 'Eduardo', category: 'Prenatal' , image: 'https://via.placeholder.com/250' },
+    { id: 1, teacher: 'Cesar', category: 'Yoga' , image: 'https://via.placeholder.com/250' },
+    { id: 2, teacher: 'Carlos', category: 'Cycling' , image: 'https://via.placeholder.com/250' },
+    { id: 3, teacher: 'Gustavo', category: 'Yoga' , image: 'https://via.placeholder.com/250' },
+    { id: 4, teacher: 'Carlos', category: 'Yoga' , image: 'https://via.placeholder.com/250' },
 
   ]
+
+  public videoCourses: IVideoCourses[]  = [
+    {
+      id: 1,
+      title: 'How to get started with a healthy lifestyle',
+      description: 'A healthy lifestyle is one which helps to keep and improve your health and well-being. There are many different things that you can do to live a healthy lifestyle, such as eating healthy, being physically active, maintaining a healthy weight, and managing your stress.',
+      thumbnail: 'https://via.placeholder.com/250',
+      videoUrl: 'https://www.youtube.com/watch?v=8A89M3nR2oY'
+    },
+    {
+      id: 2,
+      title: 'How to get started with a healthy lifestyle',
+      description: 'A healthy lifestyle is one which helps to keep and improve your health and well-being. There are many different things that you can do to live a healthy lifestyle, such as eating healthy, being physically active, maintaining a healthy weight, and managing your stress.',
+      thumbnail: 'https://via.placeholder.com/250',
+      videoUrl: 'https://www.youtube.com/watch?v=8A89M3nR2oY'
+    },
+    {
+      id: 3,
+      title: 'How to get started with a healthy lifestyle',
+      description: 'A healthy lifestyle is one which helps to keep and improve your health and well-being. There are many different things that you can do to live a healthy lifestyle, such as eating healthy, being physically active, maintaining a healthy weight, and managing your stress.',
+      thumbnail: 'https://via.placeholder.com/250',
+      videoUrl: 'https://www.youtube.com/watch?v=8A89M3nR2oY'
+    },
+    {
+      id: 4,
+      title: 'How to get started with a healthy lifestyle',
+      description: 'A healthy lifestyle is one which helps to keep and improve your health and well-being. There are many different things that you can do to live a healthy lifestyle, such as eating healthy, being physically active, maintaining a healthy weight, and managing your stress.',
+      thumbnail: 'https://via.placeholder.com/250',
+      videoUrl: 'https://www.youtube.com/watch?v=8A89M3nR2oY'
+    }
+  ];
+
+  public latestBlogs: ILatestBlogs[]  = [
+    { id: 1, name: 'Cesar', description: 'Yoga' , image: 'https://via.placeholder.com/250' },
+    { id: 2, name: 'Carlos', description: 'Cycling' , image: 'https://via.placeholder.com/250' },
+  ];
 
   constructor(private router:Router, private route:ActivatedRoute) {
     this.route.queryParams.subscribe((params: { [key: string]: any }) => {
--- src/app/home/pages/profile-page/profile-page.component.ts
+++ src/app/home/pages/profile-page/profile-page.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterLink, Router, ActivatedRoute } from '@angular/router';
+import { CarruselBgImgComponent } from '../../components/carrusel-bg-img/carrusel-bg-img.component';
+
+interface IPopularCourses {
+  id: number;
+  teacher: string;
+  category: string;
+  image: string;
+}
+
 
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.css',
   standalone: true,
-  imports: [ RouterLink ]
+  imports: [ RouterLink, CommonModule, CarruselBgImgComponent]
 })
-export class ProfilePageComponent { }
+export class ProfilePageComponent {
+
+  public popularCourses: IPopularCourses[] = [
+    { id: 1, teacher: 'Cesar', category: 'Yoga' , image: 'https://via.placeholder.com/250' },
+    { id: 2, teacher: 'Carlos', category: 'Cycling' , image: 'https://via.placeholder.com/250' },
+    { id: 3, teacher: 'Gustavo', category: 'Yoga' , image: 'https://via.placeholder.com/250' },
+    { id: 4, teacher: 'Carlos', category: 'Yoga' , image: 'https://via.placeholder.com/250' },
+
+  ]
+  
+ }
