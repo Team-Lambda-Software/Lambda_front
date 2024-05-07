7f54bde77f47bc53982acbfe6efdac29fa017210
highwir3 <hollowsouls2001@gmail.com>
2024-05-07 13:48:15 -0400

    added chart in profile and transloco, fixed dark mode circular bar
    install ngx charts

--- src/app/home/pages/profile-page/profile-page.component.ts
+++ src/app/home/pages/profile-page/profile-page.component.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { TranslocoModule } from '@jsverse/transloco';
 import { CarruselBgImgComponent } from '../../components/carrusel-bg-img/carrusel-bg-img.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { Color, ScaleType } from '@swimlane/ngx-charts';
+
 
 interface IPopularCourses {
   id: number;
@@ -11,14 +14,57 @@ interface IPopularCourses {
   image: string;
 }
 
+
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.css',
   standalone: true,
-  imports: [ RouterLink, CommonModule, CarruselBgImgComponent, TranslocoModule]
+  imports: [ RouterLink, CommonModule, CarruselBgImgComponent, TranslocoModule, NgxChartsModule]
 })
 export class ProfilePageComponent {
+
+  chartData = [
+    {
+      name: 'Mon',
+      value: 100
+    },
+    {
+      name: 'Tue',
+      value: 200
+    },
+    {
+      name: 'Wed',
+      value: 150
+    },
+    {
+      name: 'Thu',
+      value: 300
+    },
+    {
+      name: 'Fri',
+      value: 250
+    },
+    {
+      name: 'Sat',
+      value: 180
+    },
+    {
+      name: 'Sun',
+      value: 220
+    }
+  ];
+
+  colorScheme: Color = {
+    name: 'myScheme',
+    selectable: true,
+    group: ScaleType.Ordinal,
+    domain: ['#4F14A0', '#27AE60']
+  };
+
+  onBarSelect(event: any) {
+    console.log(event);
+  }
 
   public popularCourses: IPopularCourses[] = [
     { id: 1, teacher: 'Cesar', category: 'Yoga' , image: 'https://via.placeholder.com/250' },
@@ -27,5 +73,7 @@ export class ProfilePageComponent {
     { id: 4, teacher: 'Carlos', category: 'Yoga' , image: 'https://via.placeholder.com/250' },
 
   ]
+
+  
 
  }
