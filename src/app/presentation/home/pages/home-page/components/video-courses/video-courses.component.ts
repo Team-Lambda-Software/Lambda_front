bea7ad45b31eafa17850cff90a73942ea1e5b532
Paul Gamboa <pagamboa.19@est.ucab.edu.ve>
2024-06-09 21:58:33 -0400

    feat: adding video courses to home page

---
+++ src/app/presentation/home/pages/home-page/components/video-courses/video-courses.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { CourseUsecaseProvider } from '../../../../../../core/course/infrastructure/providers/course-usecase-provider';
+import { finalize } from 'rxjs';
+import { PlayerCardComponent } from '../../../../components/player-card/player-card.component';
+import { IPlayerCard } from '../../../../interfaces/IPlayerCard';
+import { PartialCourseToPlayerCard } from '../../../../adapters/PlayerCardAdapter';
+import { TranslocoModule } from '@jsverse/transloco';
+import { RouterLink } from '@angular/router';
+import { SquareSkeletonComponent } from '../../../../../shared/components/square-skeleton/square-skeleton.component';
+
+@Component({
+  selector: 'app-video-courses',
+  standalone: true,
+  imports: [
+    PlayerCardComponent,
+    TranslocoModule,
+    RouterLink,
+    SquareSkeletonComponent
+  ],
+  templateUrl: './video-courses.component.html',
+  styleUrl: './video-courses.component.css'
+})
+export class VideoCoursesComponent implements OnInit {
+
+  public courseUseCase = inject(CourseUsecaseProvider);
+  public courses = signal<IPlayerCard[]>([]);
+  public isLoading = false;
+
+  ngOnInit(): void {
+    this.getCourses();
+  }
+
+  getCourses() {
+    this.isLoading = true;
+    this.courseUseCase.usecase.getCoursesByParams('?filter=RECENT&perPage=10')
+      .pipe(finalize(() => this.isLoading = false))
+      .subscribe((courses) => {
+        this.courses.set( courses.map(c => PartialCourseToPlayerCard(c)));
+      })
+  }
+}
