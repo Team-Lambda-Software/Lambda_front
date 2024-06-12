555de5fa9c2c2de4a2a50f7a8fb7f620e1450bee
C102002 <alfnicolasfff@hotmail.com>
2024-06-11 22:55:11 -0400

    fix:Result import into proxy-use-case

--- src/app/core/search/application/proxy-search-use-case.service.ts
+++ src/app/core/search/application/proxy-search-use-case.service.ts
@@ -1,19 +1,19 @@
 import { Observable, of, tap } from 'rxjs';
 import { SearchModel } from '../domain/search-model';
 import { ISearchUseCase } from '../domain/interfaces/search-use-case.interface';
-import { Result } from '../../helpers/Result';
+import { Result } from '../../../common/helpers/Result';
 
 export class ProxySearchUseCase implements ISearchUseCase {
-  
+
   private cacheProxy = {
     getBySearch: new Map<string, Result<Observable<SearchModel>>>(),
   }
 
   constructor(private _searchUseCaseService: ISearchUseCase) { }
-  
+
   getBySearch(terms: string | undefined): Result<Observable<SearchModel>> {
     if ( this.cacheProxy.getBySearch.has(terms ?? '*') ) return this.cacheProxy.getBySearch.get(terms ?? '*')!
-    else if (!this._searchUseCaseService.getBySearch(terms).isError()) 
+    else if (!this._searchUseCaseService.getBySearch(terms).isError())
       return Result.makeResult(this._searchUseCaseService.getBySearch(terms).getValue()
               .pipe(tap(c => {
                 this.cacheProxy.getBySearch.set(terms ?? '*', Result.makeResult<Observable<SearchModel>>(of(c)))
