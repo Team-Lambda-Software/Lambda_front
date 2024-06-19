3eacf3f0ee7d34d15e75338064b624dd8fcc38bc
Paul Gamboa <pagamboa.19@est.ucab.edu.ve>
2024-06-19 13:07:26 -0400

    feat: inifite scroll on blogs by category

--- src/app/presentation/home/pages/blogs-pages/main-blogs/main-blogs.component.ts
+++ src/app/presentation/home/pages/blogs-pages/main-blogs/main-blogs.component.ts
@@ -6,12 +6,13 @@ import { CardCarruselComponent } from '../../../components/card-carrusel/card-ca
 import { LitleCardComponent } from '../../../components/litle-card/litle-card.component';
 import { Category } from '../../../../../core/categories/domain/category.model';
 import { CategoyUseCaseProvider } from '../../../../../core/categories/infrastructure/providers/category-usecase-provider';
-import { finalize, map } from 'rxjs';
+import { finalize, map, tap } from 'rxjs';
 import { BlogUsecaseProvider } from '../../../../../core/blog/infrastructure/providers/blog-usecase-provider';
 import { PartialBlogToICardAdapter } from '../../../adapters/BlogAdapter';
 import { ICard } from '../../../interfaces/ILittleCard';
 import { SquareSkeletonComponent } from '../../../../shared/components/square-skeleton/square-skeleton.component';
 import { RecentPostComponent } from './components/recent-post/recent-post.component';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 @Component({
   selector: 'app-main-blogs',
@@ -23,7 +24,8 @@ import { RecentPostComponent } from './components/recent-post/recent-post.compon
     CardCarruselComponent,
     LitleCardComponent,
     SquareSkeletonComponent,
-    RecentPostComponent
+    RecentPostComponent,
+    InfiniteScrollModule
   ],
   templateUrl: './main-blogs.component.html',
   styleUrl: './main-blogs.component.css'
@@ -39,12 +41,15 @@ export class MainBlogsComponent implements OnInit {
   public isLoadingBlogsByCategory = signal(false);
   public isLoadingCategories = signal(false);
   public selectedCategory?: Category;
+  public blogNextPage = 0;
+  public isLoadingScroll = signal(false);
 
   constructor(@Inject(DOCUMENT) private document: Document) {
     this.route.queryParams.subscribe((params: { [key: string]: any }) => {
       if(params['category']) {
+        this.blogNextPage = 0;
         this.selectedCategory = this.categories().find(c => c.name === params['category']);
-        const baseQuery = '?filter=RECENT';
+        const baseQuery = '?filter=RECENT&perPage=5&page=' + this.blogNextPage;
         const categoryQuery = this.selectedCategory?.id ? `&category=${this.selectedCategory.id}` : '';
         const queryParams = `${baseQuery}${categoryQuery}`;
         this.getBlogs(queryParams);
@@ -85,5 +90,21 @@ export class MainBlogsComponent implements OnInit {
         map(b => b.map(PartialBlogToICardAdapter)),
         finalize(() => this.isLoadingBlogsByCategory.set(false))
       ).subscribe(b => this.blogsByCategory.set(b))
+  }
+
+  //TODO: OJO CON LA PAGINACIÓN, HAY QUE DECIDIR TODOS LOS EQUIPOS EL FUNCIONAMIENTO DEL MISMO
+  //TODO: PUEDE SER LIMIT, OFFSET, PAGE, PERPAGE, ETC
+  onScroll() {
+    this.isLoadingScroll.set(true);
+    const baseQuery = '?filter=RECENT&perPage=5&page=' + (this.blogNextPage + 1);
+    const categoryQuery = this.selectedCategory?.id ? `&category=${this.selectedCategory.id}` : '';
+    const queryParams = `${baseQuery}${categoryQuery}`;
+    console.log(queryParams)
+    this.blogUseCaseService.usecase.getByParams(queryParams)
+      .pipe(
+        tap(() => this.blogNextPage++),
+        map(b => b.map(PartialBlogToICardAdapter)),
+        finalize(() => this.isLoadingScroll.set(false))
+      ).subscribe(b => this.blogsByCategory.set([...this.blogsByCategory(), ...b]))
   }
 }
