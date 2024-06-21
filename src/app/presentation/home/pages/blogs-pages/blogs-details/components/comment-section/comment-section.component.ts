28c32f41689a544ddd51c016d831a7308a90ebda
Paul Gamboa <pagamboa.19@est.ucab.edu.ve>
2024-06-21 13:50:14 -0400

    add: infinite scroll on comments on blog details page

--- src/app/presentation/home/pages/blogs-pages/blogs-details/components/comment-section/comment-section.component.ts
+++ src/app/presentation/home/pages/blogs-pages/blogs-details/components/comment-section/comment-section.component.ts
@@ -4,29 +4,59 @@ import { IComment } from '../../../../../../../core/comments/domain/comment.mode
 import { TranslocoModule } from '@jsverse/transloco';
 import { finalize } from 'rxjs';
 import { SquareSkeletonComponent } from '../../../../../../shared/components/square-skeleton/square-skeleton.component';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-comment-section',
   standalone: true,
-  imports: [TranslocoModule, SquareSkeletonComponent],
+  imports: [
+    TranslocoModule,
+    SquareSkeletonComponent,
+    InfiniteScrollModule
+  ],
   templateUrl: './comment-section.component.html',
   styleUrl: './comment-section.component.css'
 })
 export class CommentSectionComponent implements OnInit {
-  @Input({required: true}) blogId!: string;
+  @Input({required: true}) 
+  public blogId!: string;
+  
+  private currentPage = 1;
   private commentUseCase = inject(CommentsUseCaseProvider);
   public comments = signal<IComment[]>([]);
   public isLoadingComments = signal<boolean>(false);
+  public isLoadingMoreComments = signal<boolean>(false);
+  public document = inject(DOCUMENT)
+  public scrollContainer: HTMLElement | null = null;
+  public hasNoMoreComments = signal<boolean>(false);
 
   ngOnInit() {
+    this.scrollContainer = this.document.getElementById('scrollBlogCommentContainer');
     this.getComments()
   }
 
   getComments() {
-    this.isLoadingComments.set(true);
+    if(this.currentPage === 1) this.isLoadingComments.set(true);
+    else this.isLoadingMoreComments.set(true);
     this.commentUseCase.usecase
-      .getCommentsByParams(`?perPage=10&page=0&blog=${this.blogId}`)
-      .pipe(finalize(() => this.isLoadingComments.set(false)))
-      .subscribe(this.comments.set);
+      .getCommentsByParams(`?perPage=5&page=${this.currentPage}&blog=${this.blogId}`)
+      .pipe(
+        finalize(() => {
+          this.isLoadingComments.set(false);
+          this.isLoadingMoreComments.set(false);
+          this.currentPage++;
+        })
+      )
+      .subscribe( (comment) => {
+        if(comment.length === 0) this.hasNoMoreComments.set(true);
+        this.comments.set([...this.comments(), ...comment]);
+      });
   }
+
+  onClick() {
+    console.log(this.document.getElementById('scrollBlogCommentContainer'));
+  }
+
 }
+
