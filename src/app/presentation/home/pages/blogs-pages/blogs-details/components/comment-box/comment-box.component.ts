40d8600464549209540173575b46506e8078db97
Paul Gamboa <pagamboa.19@est.ucab.edu.ve>
2024-06-21 15:20:18 -0400

    fix: Display info in correct language

--- src/app/presentation/home/pages/blogs-pages/blogs-details/components/comment-box/comment-box.component.ts
+++ src/app/presentation/home/pages/blogs-pages/blogs-details/components/comment-box/comment-box.component.ts
@@ -1,10 +1,10 @@
 import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild, inject, signal } from '@angular/core';
 import { CommentsUseCaseProvider } from '../../../../../../../core/comments/infraestructure/providers/category-use-case-provider';
 import { CommentFeatureDto, ETarget, IComment } from '../../../../../../../core/comments/domain/comment.model';
-import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatSnackBar, MatSnackBarConfig, MatSnackBarModule } from '@angular/material/snack-bar';
 import { delay } from 'rxjs';
-import { TranslocoModule } from '@jsverse/transloco';
+import { TranslocoModule, TranslocoService } from '@jsverse/transloco';
 import { UserStatusService } from '../../../../../../../core/user/infraestructure/services/user-status.service';
 
 const SNACKBAR_OPTIONS: MatSnackBarConfig = { duration: 4000, verticalPosition: 'top', horizontalPosition: 'center'};
@@ -26,26 +26,29 @@ export class CommentBoxComponent implements OnInit {
   private userStatusService = inject(UserStatusService);
   public newComment = new FormControl('');
   public isLoadingComment = signal<boolean>(false);
+  public translocoService = inject(TranslocoService);
 
   ngOnInit(): void {
     this.newComment.valueChanges.subscribe(() => this.resizeInput());
   }
   
   sendComment() {
-    if(!this.newComment.value) return this.snackBar.open('Comment can not be empty', 'Ok', SNACKBAR_OPTIONS);
+    const isLanguageEnglish = this.translocoService.getActiveLang() === 'en';
+    if(!this.newComment.value) return this.snackBar.open( isLanguageEnglish ? 'Comment can not be empty' : 'Comentario no puede estar vacío', 'Ok', SNACKBAR_OPTIONS);
     const newComment = this.newComment.value;
     this.resizeInput();
-    this.newComment.setValue('Sending message...');
+    this.newComment.setValue( isLanguageEnglish ? 'Sending message...' : 'Enviando mensaje...');
     this.newComment.disable();
     this.isLoadingComment.set(true);
     let comment = new CommentFeatureDto(this.blogId, ETarget.blog, newComment)
     this.commentUseCase.usecase.createComment(comment)
-      .pipe(delay(2000))
       .subscribe({
         next: () => {
+          const message = isLanguageEnglish ? 'Comment created' : 'Comentario creado';
+          const button = isLanguageEnglish ? 'Close' : 'Cerrar';
           this.isLoadingComment.set(false);
           this.newComment.enable();
-          this.snackBar.open('Comment created', 'Close', SNACKBAR_OPTIONS);
+          this.snackBar.open(message, button, SNACKBAR_OPTIONS);
           this.newComment.setValue('');
           if(this.userStatusService.currentUser().hasValue()) {
             const username = this.userStatusService.currentUser().getValue().name;
@@ -53,10 +56,11 @@ export class CommentBoxComponent implements OnInit {
           }
         },
         error: () => {
+          const message = isLanguageEnglish ? 'A error happened creating the comment' : 'Ocurrió un error creando el comentario';
           this.isLoadingComment.set(false);
           this.newComment.enable();
           this.newComment.setValue(newComment);
-          this.snackBar.open('A error happened creating the comment', 'Ok', SNACKBAR_OPTIONS);
+          this.snackBar.open(message, 'Ok', SNACKBAR_OPTIONS);
         }
       
       });
