2c53e713488e7a5e443ae0e74ba990c3802e70a6
C102002 <alfnicolasfff@hotmail.com>
2024-07-09 13:10:30 -0400

    add: popup when delete and improve a litle notification page

--- src/app/presentation/home/pages/notification-page/notification-page.component.ts
+++ src/app/presentation/home/pages/notification-page/notification-page.component.ts
@@ -5,27 +5,33 @@ import { TranslocoModule } from '@jsverse/transloco';
 import { NotificationUseCaseProvider } from '../../../../core/notification/infrastructure/providers/notification-usecase-provider';
 import { Notification } from '../../../../core/notification/domain/notification.model';
 import { StructureNotificationComponent } from './structure-notification/structure-notification.component';
+import { PopupInfoModalService } from '../../../shared/services/popup-info-modal/popup-info-modal.service';
+import { LoaderComponent } from '../../../auth/components/loader/loader.component';
+import { DarkModeService } from '../../../shared/services/dark-mode/dark-mode.service';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-notification-page',
   standalone: true,
-  imports: [BasicHeaderNotificationComponent,RouterLink, TranslocoModule, StructureNotificationComponent],
+  imports: [BasicHeaderNotificationComponent,RouterLink, TranslocoModule, StructureNotificationComponent,LoaderComponent,CommonModule],
   templateUrl: './notification-page.component.html',
   styleUrl: './notification-page.component.css'
 })
 export class NotificationPageComponent {
-  
+
   private notificationUseCase = inject(NotificationUseCaseProvider)
+  public darkModeService = inject(DarkModeService);
   public structure: Notification[] = [];
+  public notificationIsloading:boolean=false
 
   ngOnInit(): void {
-
+    this.notificationIsloading=true
     this.notificationUseCase.usecase.getNotificationByParams('?page=0&perPage=0')
       .subscribe(notifications => {
         this.structure.push(...notifications);
         this.sortNotificationsByDate();
-
+        this.notificationIsloading=false
   });
  }
 
@@ -38,7 +44,7 @@ export class NotificationPageComponent {
   }
 
   public clearNotifications(): void {
-    this.structure = []; 
+    this.structure = [];
   }
 }
 
