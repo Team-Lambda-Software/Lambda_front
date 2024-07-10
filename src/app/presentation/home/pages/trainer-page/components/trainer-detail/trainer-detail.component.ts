27b709eea7646074ce6d45174fb0561ce0b8c586
Paul Gamboa <pagamboa.19@est.ucab.edu.ve>
2024-07-09 23:09:02 -0400

    feat: adding skeleton on trainer page

--- src/app/presentation/home/pages/trainer-page/components/trainer-detail/trainer-detail.component.ts
+++ src/app/presentation/home/pages/trainer-page/components/trainer-detail/trainer-detail.component.ts
@@ -2,24 +2,31 @@ import { Component, EventEmitter, inject, input, Input, Output, signal } from '@
 import { TranslocoModule } from '@jsverse/transloco';
 import { TrainerDetail } from '../../../../../../core/trainer/domain/trainer.model';
 import { TrainerFollowToggleProvider } from '../../../../../../core/trainer/infrastructure/providers/trainer-follow-toggle-use-case';
+import { SquareSkeletonComponent } from '../../../../../shared/components/square-skeleton/square-skeleton.component';
+import { CircleSkeletonComponent } from '../../../../../shared/components/circle-skeleton/circle-skeleton.component';
 
 @Component({
   selector: 'app-trainer-detail',
   standalone: true,
-  imports: [TranslocoModule],
+  imports: [
+    TranslocoModule,
+    SquareSkeletonComponent,
+    CircleSkeletonComponent
+  ],
   templateUrl: './trainer-detail.component.html',
   styleUrl: './trainer-detail.component.css'
 })
 export class TrainerDetailComponent {
+
   @Output() public onTrainerToggleFollow = new EventEmitter<void>();
   private trainerFollowToggleProvider = inject(TrainerFollowToggleProvider);
-  public trainerDetail = input.required<TrainerDetail>();
+  public trainerDetail = input.required<TrainerDetail | null>();
   public isLoading = signal(false);
 
   onToggleFollow() {
-    const prevValue = this.trainerDetail().userFollow;
+    const prevValue = this.trainerDetail()!.userFollow;
     this.isLoading.set(true);
-    this.trainerFollowToggleProvider.usecase.execute(this.trainerDetail().id)
+    this.trainerFollowToggleProvider.usecase.execute(this.trainerDetail()!.id)
       .subscribe( (response) => {
         this.isLoading.set(false);
         this.onTrainerToggleFollow.emit();
