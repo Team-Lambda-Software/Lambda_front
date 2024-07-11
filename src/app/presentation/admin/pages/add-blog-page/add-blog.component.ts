41a209883c9a00b48f01f0d1dc66976c2e72cf20
C102002 <alfnicolasfff@hotmail.com>
2024-07-11 10:47:03 -0400

    fix: popup error display

--- src/app/presentation/admin/pages/add-blog-page/add-blog.component.ts
+++ src/app/presentation/admin/pages/add-blog-page/add-blog.component.ts
@@ -106,7 +106,7 @@ export class AddBlogPageComponent {
         },
         error:(error:Result<Error>)=>{
           this.isLoadingAddCourse=false
-          this.popupService.displayErrorModal(this.BlogCreatedSucsessfully)
+          this.popupService.displayInfoModal(this.BlogCreatedSucsessfully)
         },
       })
     }
